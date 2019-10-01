81aaaef72a596f9bf4a0ff03784b7284a80cc667
0778364 <0778364@KELCE-LABS33-P.pittstate.edu>
2019-10-01 14:19:24 -0500

    Finished circle class.

--- src/main/java/Circle.java
+++ src/main/java/Circle.java
@@ -1,17 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+import java.util.Scanner;
 /**
  *
  * @author 0778364
  */
 public class Circle {
     
-    public static void main(String[] args) {
-        
-        
+    private double radius;
+    private final double pi = 3.14159;
+    
+    Circle(double radius){
+        this.radius = radius;
     }
+    
+    Circle(){
+        this.radius = 0.0;
+    }
+    
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("What is your circle's radius?");
+        double inputRadius = Double.parseDouble(keyboard.nextLine());
+        
+        Circle myCircle = new Circle(inputRadius);
+        System.out.println("Your circle's area is: " + myCircle.getArea());
+        System.out.println("Your circle's diameter is: " + myCircle.getDiameter());
+        System.out.println("Your circle's circumference is: " + myCircle.getCircumference());
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    
+    public double getArea(){
+        return this.radius * this.radius * this.pi;
+    }
+    
+    public double getDiameter(){
+        return this.radius * 2;
+    }
+    public double getCircumference(){
+        return this.radius * this.pi * 2;
+    }
+    
 }
